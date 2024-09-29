d2772dabf4a488d8c7a0aba6aecf4ab5e247ebb6
Neerajbhatt121 <neerajbhatt1912@gmail.com>
2024-09-29 20:53:25 +0530

    GetAllPosts, GetPostofUser is created

--- backend/Controllers/postController.js
+++ backend/Controllers/postController.js
@@ -17,16 +17,13 @@ export const createPostController = async (req, res) => {
         description = "This is the default description";
 
       case photo && photo.size > 1000000:
-        
-        
     }
 
     const Post = new postModal({
-        author: new mongoose.Types.ObjectId(author),
+      author: new mongoose.Types.ObjectId(author),
       description,
       likes,
       totallikes,
-     
     });
     if (photo) {
       Post.photo.data = fs.readFileSync(photo.path);
@@ -46,3 +43,58 @@ export const createPostController = async (req, res) => {
     });
   }
 };
+
+//----------------------//
+// get all posts
+export const getAllPosts = async (req, res) => {
+  try {
+    const  Posts  = await postModal.find();
+    if (Posts.length > 0) {
+      res.status(200).send({
+        sucess: true,
+        message: "Get all posts",
+        Posts
+      });
+    } else{
+      res.status(404).send({
+        success: false,
+        message: "No posts found for this user",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      sucess: false,
+      message: "Error while getting the post",
+    });
+  }
+};
+
+
+//-------------------------//
+// Get Posts of Particular user
+export const getPostsOfUser = async (req, res) => {
+  const UserId = req.params.userId;
+  console.log("userId is this", UserId);
+    try {
+        const Posts = await postModal.find({author: UserId})
+        if(Posts.length > 0){
+          res.status(200).send({
+            sucess:true,
+            message: "Get Posts of Id",
+            Posts
+          })
+        } else {
+          res.status(404).send({
+            success: false,
+            message: "No posts found for this user",
+          });
+        }
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({
+        sucess: false,
+        message: "Error while getting the post",
+      });
+    }
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import formidable from "express-formidable";
+import { getAllPosts, getPostsOfUser } from '../Controllers/postController.js';
 import { requireSignIn } from '../middleware/authMiddleware.js';
 
 import { createPostController } from '../Controllers/postController.js';
@@ -10,5 +11,13 @@ const router = express.Router();
 // --------------------//
 // Create a new post 
 router.post('/create-post',requireSignIn ,formidable(), createPostController)
+
+//---------------------//
+// Get all posts
+router.get('/get/allPosts', getAllPosts)
+
+//---------------------//
+// Get all posts of user
+router.get('/get/user-posts/:userId', getPostsOfUser)
 
 export default router;
